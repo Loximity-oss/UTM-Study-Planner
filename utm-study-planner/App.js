b6cbf3732bdb3af826730e888a7aa130b6f4d5f3
Danial <nizwien@gmail.com>
2022-04-16 21:36:48 +0800

    Update App.js

--- utm-study-planner/App.js
+++ utm-study-planner/App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#FFFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
