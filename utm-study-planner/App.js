67c86faf320eb654d8a6398b38fa6e2bec885017
Loximity-oss <67264198+Loximity-oss@users.noreply.github.com>
2022-04-16 21:35:13 +0800

    Update App.js

--- utm-study-planner/App.js
+++ utm-study-planner/App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
