4062ff36121bd1b595f1ba14f91d3ddef7980e9e
Danial <nizwien@gmail.com>
2022-04-17 19:58:54 +0800

    Hide Status Bar

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
-    
         <View style={styles.container}>
+            <StatusBar hidden={true}/>
             <Text>seggsaa</Text>
         </View>
     );
