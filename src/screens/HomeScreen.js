5c77ae324073ee0edcaf143d246b6890e3271a32
Loximity-oss <67264198+Loximity-oss@users.noreply.github.com>
2022-04-21 23:33:17 +0800

    Restructured Directories.

--- src/screens/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function HomeScreen(props) {
-    return (
-        <Text>HI BITCH - 2</Text>
-    );
-}
-
-export default HomeScreen;
---
+++ utmstudyplanner_mobile/android/app/src/main/java/com/utmstudyplanner_mobile/utmstudyplanner_mobile/MainActivity.java
@@ -0,0 +1,6 @@
+package com.utmstudyplanner_mobile.utmstudyplanner_mobile;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
