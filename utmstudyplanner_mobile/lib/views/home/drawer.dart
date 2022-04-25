76f406c75c54c0e2669b58d9df75eb6c7bc3a80f
Loximity-oss <67264198+Loximity-oss@users.noreply.github.com>
2022-04-26 00:11:06 +0800

    Completed Homescreen.

---
+++ utmstudyplanner_mobile/lib/views/home/drawer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class Drawer{
+  static getDrawer(){
+    return Drawer(
+      child:
+    )
+  }
+}
