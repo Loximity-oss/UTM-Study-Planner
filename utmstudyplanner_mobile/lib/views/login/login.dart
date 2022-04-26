9f086b47c5200803cdc6f3f0e83279981a37928a
Loximity-oss <67264198+Loximity-oss@users.noreply.github.com>
2022-04-27 01:52:03 +0800

    modified login screen

--- utmstudyplanner_mobile/lib/views/login/login.dart
+++ utmstudyplanner_mobile/lib/views/login/login.dart
@@ -22,111 +22,103 @@ class _loginPageState extends State<loginPage> {
     return firstTimeState
         ? const IntroductionPage()
         : Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          reverse: true,
-          padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 350,
-              height: 350,
-              child: Image.asset('assets/signup/mobile_3.png'),
-            ),
-            Padding(padding: EdgeInsets.only(
-              left: 50,
-              right: 50,
-              bottom: 10,
-            ),
-              child: TextField(
-                controller: usernameInput,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                    hintText: 'Username',
-                    hintStyle: TextStyle(color: Colors.orange),
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red)
+      backgroundColor: const Color.fromARGB(255, 255, 249, 235),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 120, bottom: 100),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 300,
+                height: 300,
+                child: Image.asset('assets/signup/mobile_1.png'),
+              ),
+              //Username and Password Containers
+              Container(
+                padding: const EdgeInsets.only(left: 50, right: 50),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 5),
+                    const Text('Welcome!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    const Text('UTM Study Planner System', style: TextStyle(fontSize: 15)),
+                    const SizedBox(height: 20),
+                    TextField(controller: usernameInput,
+                      style: const TextStyle(fontSize: 14),
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.only(left: 20, right: 20),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: const BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          filled: true,
+                          hintStyle: TextStyle(color: Colors.grey[800]),
+                          hintText: "Username",
+                          fillColor: Colors.white),
+                      ),
+                    const SizedBox(height: 10),
+                    TextField(controller: passwordInput,
+                      style: const TextStyle(fontSize: 14),
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.only(left: 20, right: 20),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: const BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          filled: true,
+                          hintStyle: TextStyle(color: Colors.grey[800]),
+                          hintText: "Password",
+                          fillColor: Colors.white),
                     ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red)
-                    )
-                ),
-              ),
-            ),
-
-
-            Padding(padding: EdgeInsets.only(
-              left: 50,
-              right: 50,
-              bottom: 20,
-            ),
-              child: TextField(
-                controller: passwordInput,
-                obscureText: true,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-                cursorColor: Colors.black,
-                decoration: const InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: TextStyle(color: Colors.orange),
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red)
+                    TextButton(
+                      child:
+                      Text("Don't have an account? Click me!", style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold)),
+                      onPressed: () {
+                        print('Pressed');
+                        Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => registerPage()),
+                        );
+                      },
                     ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red)
-                    )
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2.0,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(35.0),
+                        color: const Color.fromARGB(255, 93, 6, 29),
+                      ),
+                      child: MaterialButton(
+                        onPressed: () {
+                          print('Username: '+usernameInput.text);
+                          print('Password: '+passwordInput.text);
+
+                          //Routing to a different page
+                          Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => homepage()),
+                          );
+                        },
+                        child: Text('Login',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),),
+
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-
-            Container(
-              width: MediaQuery.of(context).size.width / 2.0,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(35.0),
-                color: Colors.red[900],
-              ),
-              child: MaterialButton(
-                onPressed: () {
-                  print('Username: '+usernameInput.text);
-                  print('Password: '+passwordInput.text);
-
-                  //Routing to a different page
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => homepage()),
-                  );
-                },
-                child: Text('Login',
-                    style: TextStyle(
-                      fontSize: 19,
-                      color: Colors.white,
-                  ),),
-
-              ),
-            ),
 
 
-            TextButton(
-              child:
-              Text("Don't have an account? Click me!"),
-              onPressed: () {
-                print('Pressed');
-                Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => registerPage()),
-                );
-              },
-            ),
 
-          ],
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
