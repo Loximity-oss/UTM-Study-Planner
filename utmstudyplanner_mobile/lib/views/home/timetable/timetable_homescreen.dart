539b2ce48ef6f28a052e07c9fea36bd804d0bacb
Danial <nizwien@gmail.com>
2022-06-21 00:52:34 +0800

    Updated Timetable

--- utmstudyplanner_mobile/lib/views/home/timetable/timetable_homescreen.dart
+++ utmstudyplanner_mobile/lib/views/home/timetable/timetable_homescreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:utmstudyplanner_mobile/views/home/drawer.dart';
 import '../../../server/conn.dart';
@@ -13,6 +14,24 @@ class Timetable extends StatefulWidget{
 
 class _TimetablePageState extends State<Timetable>{
 
+  final CalendarController _calendarController = CalendarController();
+  final DateTime today = DateTime.now();
+  late DateTime firstDayOfTheWeek;
+  late DateTime lastDayOfTheWeek;
+
+  @override
+  void initState(){
+    _testing();
+    super.initState();
+  }
+
+  _testing(){
+    var weekDay = today.weekday;
+
+    firstDayOfTheWeek = today.subtract(Duration(days: weekDay));
+    lastDayOfTheWeek = today.subtract(Duration(days: weekDay - 5));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,14 +41,16 @@ class _TimetablePageState extends State<Timetable>{
         backgroundColor: const Color.fromRGBO(93, 6, 29, 1.0),
       ),
       body: SfCalendar(
-        view: CalendarView.week,
+        view: CalendarView.workWeek,
+        controller: _calendarController,
         timeSlotViewSettings: const TimeSlotViewSettings(
-          startHour: 6,
-          endHour: 18,
+          startHour: 7,
+          endHour: 20,
+          nonWorkingDays: <int>[DateTime.friday, DateTime.saturday],
+          timeIntervalHeight: -1,
+          timeIntervalWidth: -1,
         ),
       ),
     );
   }
-
-
 }
